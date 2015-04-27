d806c31040b5b07ec5c234afac1017ccb24555dc
Cesar Chen <cesar6134@gmail.com>
2015-04-27 11:52:10 +0800

    Modified the play time of SVG animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,12 @@
 (function () {
+
   if ($('#signature').length > 0) {
-    var signature = new Vivus('signature', {type: 'oneByOne', duration: 200}, function (event) {
+    var signature = new Vivus('signature', {type: 'oneByOne', duration: 200, start: 'manual'}, function (event) {
       $('#signature path').css('fill', '#fff');
     });
+    setTimeout( function () {
+      $('#signature').css('visibility', 'visible');
+      signature.play(0);
+    }, 1000);
   }
 })();
