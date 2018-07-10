c564c343c8c43b643ad82581dc8c23d1e9414336
Cesar Chen <cesar6134@gmail.com>
2018-07-10 11:54:21 +0800

    refactor: Refactor the script and the page

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -55,11 +55,11 @@ const postCSSLoaderOptions = {
   // https://github.com/facebook/create-react-app/issues/2677
   ident: 'postcss',
   plugins: () => [
+    require('postcss-easy-import'),
     require('rucksack-css'),
     require('postcss-calc'),
-    require('postcss-nesting'),
     require('postcss-url'),
-    require('postcss-easy-import'),
+    require('postcss-nesting'),
     require('postcss-flexbugs-fixes'),
     autoprefixer({
       flexbox: 'no-2009',
