7fe5a946106746d4b4e21cbf6ec31d3141e088f4
Cesar Chen <cesar6134@gmail.com>
2019-12-19 00:30:30 +0800

    refactor(packages): upgrade react-app

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-import ScrollReveal from 'scrollreveal'
-import Vivus from 'vivus'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import registerServiceWorker from './registerServiceWorker'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import './styles/styles.css'
-
-window.sr = new ScrollReveal()
-
-window.sr.reveal('section')
-
-if (document.querySelectorAll('#signature').length > 0) {
-  const signature = new Vivus(
-    'signature',
-    { type: 'oneByOne', duration: 200, start: 'manual' },
-    () => {
-      const paths = document.querySelectorAll('#signature path')
-      paths.forEach(el => {
-        el.style['fill'] = '#fff'
-      })
-    }
-  )
-
-  setTimeout(() => {
-    const svg = document.querySelector('#signature')
-    svg.style['visibility'] = 'visible'
-    signature.play(0)
-  }, 1000)
-}
-
-registerServiceWorker()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
