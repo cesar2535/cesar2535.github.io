d688a14ee3292380f559576cd446d96997d317ab
Cesar Chen <cesar6134@gmail.com>
2024-11-25 23:11:09 +0800

    feat: set up og image generating for project category

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -1,12 +1,20 @@
 import { Resvg } from "@resvg/resvg-js";
 import { type CollectionEntry } from "astro:content";
 import postOgImage from "./og-templates/post";
+import projectOgImage from "./og-templates/project";
 import siteOgImage from "./og-templates/site";
 
 function svgBufferToPngBuffer(svg: string) {
   const resvg = new Resvg(svg);
   const pngData = resvg.render();
   return pngData.asPng();
+}
+
+export async function generateOgImageForProject(
+  post: CollectionEntry<"projects">
+) {
+  const svg = await projectOgImage(post);
+  return svgBufferToPngBuffer(svg);
 }
 
 export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
